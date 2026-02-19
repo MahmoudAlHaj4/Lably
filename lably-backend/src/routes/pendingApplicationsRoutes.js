88d91579d5561f39036795e3cb09b8e2f340c56c
MahmoudAlHaj4 <mahmoudalhaj509@gmail.com>
2026-02-19 22:10:05 +0200

    feat: add pending application routes

---
+++ lably-backend/src/routes/pendingApplicationsRoutes.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const { submitApplication } = require('../controllers/pendingApplicationsController')
+const upload = require('../middleware/uploadMiddleware/uploadResumeAndPortfolio')
+const router = express.Router()
+
+router.post('/submit', upload.fields([
+        { name: 'resume', maxCount: 1 },
+        { name: 'portfolio', maxCount: 5 }
+    ]), submitApplication)
+router.use((err, req, res, next) => {
+    res.status(400).json({ error: err.message })
+})
+
+module.exports = router
