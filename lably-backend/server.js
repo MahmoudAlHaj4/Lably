4bfcdc5bfc7cdedd81591b4f285e0f72759d68b4
MahmoudAlHaj4 <mahmoudalhaj509@gmail.com>
2026-02-26 21:49:16 +0200

    feat: register auth route in server.js

--- lably-backend/server.js
+++ lably-backend/server.js
@@ -7,6 +7,10 @@ app.use(express.json())
 const pendingApplicationsRoutes = require('./src/routes/pendingApplicationsRoutes')
 app.use('/api/pendingApplication', pendingApplicationsRoutes)
 
+const authRoutes = require('./src/routes/authRoutes')
+app.use('/api/auth', authRoutes)
+
+
 const PORT = process.env.PORT || 3000
 app.listen(PORT, ()=>{
     console.log(`Express server is running on http://localhost:${PORT}`)
