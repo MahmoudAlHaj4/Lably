b342eaec7886435f2fe34c42211fa175ace6928e
MahmoudAlHaj4 <mahmoudalhaj509@gmail.com>
2026-02-26 21:57:49 +0200

    feat: add auth middleware

---
+++ lably-backend/src/middleware/authMiddleware.js
@@ -0,0 +1,28 @@
+const jwt = require("jsonwebtoken")
+
+function authMiddleware (req, res ,next) {
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+
+    if(!token){
+        return res.status(401).json({message: 'No token provided'})
+    }
+    try{
+        const decode = jwt.verify(token , process.env.JWT_SECRET)
+        req.user = decode
+        next()
+    }catch(error) {
+        return res.status(401).json({message: 'Invalid token'})
+    }
+
+
+}
+
+function adminMiddleware(req,res,next) {
+    if(req.user.role !== 'admin'){
+        return res.status(403).json({message: 'Access denied'})
+    }
+    next()
+}
+
+module.exports = {authMiddleware, adminMiddleware}
