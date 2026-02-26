924ef27c2bd40bc17503258461c2796493b78d67
MahmoudAlHaj4 <mahmoudalhaj509@gmail.com>
2026-02-26 23:46:37 +0200

    feat: add employer register route

--- lably-backend/src/routes/authRoutes.js
+++ lably-backend/src/routes/authRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express')
-const { login, activateAccount } = require('../controllers/authController')
+const { login, activateAccount, employerRegister } = require('../controllers/authController')
 const router = express.Router()
 
 router.post('/login', login)
 router.put('/activate', activateAccount)
+router.post('/register', employerRegister)
 
 module.exports = router
