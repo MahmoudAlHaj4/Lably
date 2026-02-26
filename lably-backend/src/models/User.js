352db6bd8e516d4b84d783da72f4f937fd332794
MahmoudAlHaj4 <mahmoudalhaj509@gmail.com>
2026-02-26 22:48:16 +0200

    feat: add createUser and setActivationToken methods to User model

--- lably-backend/src/models/User.js
+++ lably-backend/src/models/User.js
@@ -1,4 +1,5 @@
 const pool = require('../config/database')
+const { randomUUID } = require('crypto')
 
 class User {
     static async findByEmail(email){
@@ -8,6 +9,28 @@ class User {
         const [row] = await pool.query(query, [email])
 
         return row[0]
+    }
+
+    static async createUser(userData) {
+        const query = `INSERT INTO users (id, email, password, role, is_active) VALUES (?, ?, ?, ?, ?)`
+        const id = randomUUID()
+
+        const [result] = await pool.query(query ,[
+            id,
+            userData.email,
+            userData.password,
+            userData.role,
+            false
+        ] )
+
+        return {id, email: userData.email , role: userData.role , is_active : false}
+    }
+
+    static async setActivationToken(token ,expiry, userId) {
+   
+        const query = `UPDATE users SET activation_token = ?, activation_token_expires = ? WHERE id = ?`
+        await pool.query(query, [token, expiry, userId])
+
     }
 }
 
