2c107c6bb3c218547e760fcf66bf41f4b4737e19
MahmoudAlHaj4 <mahmoudalhaj509@gmail.com>
2026-02-27 21:01:00 +0200

    feat: create EmployerProfile model with create and findByUserId methods

---
+++ lably-backend/src/models/EmployerProfile.js
@@ -0,0 +1,30 @@
+const pool = require('../config/database')
+const { randomUUID } = require('crypto')
+
+class EmployerProfile {
+    static async create(userId , profileData) {
+        const id = randomUUID()
+
+        const query = `INSERT INTO employer_profiles (id, user_id, company_name, company_description, location, website) VALUES (?, ?, ?, ?, ?, ?)`
+
+        await pool.query(query , [
+            id,
+            userId,
+            profileData.company_name,
+            profileData.company_description || null,
+            profileData.location || null,
+            profileData.website || null
+
+        ])
+        return {id, user_id: userId , ...profileData}
+    }
+
+    static async findByUserId(userId) {
+        const query = `SELECT * FROM employer_profiles WHERE user_id =?`
+        const [row] = await pool.query(query , [userId])
+        return row[0]
+    }
+
+}
+
+module.exports = EmployerProfile
