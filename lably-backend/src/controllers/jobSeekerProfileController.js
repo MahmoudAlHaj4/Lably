c6546b628a1a00c07300c274198971b2c50102ed
MahmoudAlHaj4 <mahmoudalhaj509@gmail.com>
2026-02-28 00:28:45 +0200

    feat: add update job seeker profile function to job seekerprofile controller

--- lably-backend/src/controllers/jobSeekerProfileController.js
+++ lably-backend/src/controllers/jobSeekerProfileController.js
@@ -37,4 +37,38 @@ async function createJobSeekerProfile(req, res) {
     }
 }
 
-module.exports = {createJobSeekerProfile}
+async function updateJobSeekerProfile(req, res) {
+    try{
+        const { full_name , phone , address ,about} = req.body
+        const userId = req.user.id
+        const role = req.user.role
+
+        if(role !== 'job_seeker'){
+            return res.status(403).json({message: 'You are not a Job Seeker'})
+        }
+
+        const isActive = await User.findById(userId)
+
+        if(isActive.is_active === false){
+            return res.status(400).json({message : "You must Activate your account and create a profile first"})
+        }
+
+        const checkProfile = await JobSeekerProfile.findByUserId(userId)
+        if(!checkProfile){
+            return res.status(404).json({message: "Not found"})
+        }
+
+        await JobSeekerProfile.update(userId , {
+            full_name: full_name,
+            phone: phone,
+            address: address,
+            about: about
+        })
+        return res.status(200).json({message: 'success'})
+    }catch(error) {
+        return res.status(500).json({message : error.message})
+    }
+   
+}
+
+module.exports = {createJobSeekerProfile, updateJobSeekerProfile}
