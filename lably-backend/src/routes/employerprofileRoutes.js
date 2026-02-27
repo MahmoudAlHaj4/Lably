ea4b9873e8331c79b10c1542c7f88e835f980b9a
MahmoudAlHaj4 <mahmoudalhaj509@gmail.com>
2026-02-27 22:03:41 +0200

    feat: add PUT endpoint to employer profile routes

--- lably-backend/src/routes/employerprofileRoutes.js
+++ lably-backend/src/routes/employerprofileRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const { authMiddleware } = require('../middleware/authMiddleware')
-const { createEmployerProfile } = require('../controllers/employerProfileController')
+const { createEmployerProfile, updateEmployerProfile } = require('../controllers/employerProfileController')
 const router = express.Router()
 
 router.post('/employer-profile', authMiddleware , createEmployerProfile)
+router.put('/employer-profile', authMiddleware, updateEmployerProfile)
 
 module.exports = router
