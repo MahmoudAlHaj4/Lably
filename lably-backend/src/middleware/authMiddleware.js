9b864483794e067f3a060fda68cdb1c55432a6ff
MahmoudAlHaj4 <mahmoudalhaj509@gmail.com>
2026-02-27 21:07:59 +0200

    fix: change the message from no token to unauthorized

--- lably-backend/src/middleware/authMiddleware.js
+++ lably-backend/src/middleware/authMiddleware.js
@@ -5,7 +5,7 @@ function authMiddleware (req, res ,next) {
     const token = authHeader && authHeader.split(' ')[1]
 
     if(!token){
-        return res.status(401).json({message: 'No token provided'})
+        return res.status(401).json({message: 'unauthorized'})
     }
     try{
         const decode = jwt.verify(token , process.env.JWT_SECRET)
