f2012aea4164c3eb5f9ac528eba6367d5592922b
MahmoudAlHaj4 <mahmoudalhaj509@gmail.com>
2026-02-28 00:29:20 +0200

    feat: add PUT endpoint to job seeker profile routes

--- lably-backend/src/routes/jobSeekerProfileRoutes.js
+++ lably-backend/src/routes/jobSeekerProfileRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const { authMiddleware } = require('../middleware/authMiddleware')
-const { createJobSeekerProfile } = require('../controllers/jobSeekerProfileController')
+const { createJobSeekerProfile, updateJobSeekerProfile } = require('../controllers/jobSeekerProfileController')
 const router = express.Router()
 
 
 router.post('/job-seeker-profile', authMiddleware , createJobSeekerProfile)
+router.put('/job-seeker-profile', authMiddleware , updateJobSeekerProfile)
 
 module.exports = router
