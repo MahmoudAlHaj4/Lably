3b006a59a63f78b9651aa74794259c3015db363d
MahmoudAlHaj4 <mahmoudalhaj509@gmail.com>
2026-02-28 22:41:55 +0200

    feat: apply jobSeekerMiddleware to experience routes

--- lably-backend/src/routes/experienceRoutes.js
+++ lably-backend/src/routes/experienceRoutes.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const { authMiddleware } = require('../middleware/authMiddleware')
 const { addExperience, getJobExperiences, UpdateJobExperience, GetExperience, DeleteExperience } = require('../controllers/experienceController')
+const { jobSeekerMiddleware } = require('../middleware/jobSeekerMiddleware')
 const router = express.Router()
 
-router.post('/experience', authMiddleware , addExperience)
-router.get('/experience', authMiddleware , getJobExperiences)
-router.put('/experience/:id', authMiddleware , UpdateJobExperience)
+router.post('/experience', authMiddleware,jobSeekerMiddleware , addExperience)
+router.get('/experience', authMiddleware ,jobSeekerMiddleware, getJobExperiences)
+router.put('/experience/:id', authMiddleware, jobSeekerMiddleware , UpdateJobExperience)
 router.get('/experience/:id', authMiddleware , GetExperience)
-router.delete('/experience/:id', authMiddleware , DeleteExperience)
+router.delete('/experience/:id', authMiddleware, jobSeekerMiddleware , DeleteExperience)
 
 module.exports = router
