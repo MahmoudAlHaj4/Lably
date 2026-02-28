4e6711b24461d60e3c37919d4e91ae4ead2a4bd0
MahmoudAlHaj4 <mahmoudalhaj509@gmail.com>
2026-03-01 00:29:17 +0200

    feat: add Job model with create method

---
+++ lably-backend/src/models/Job.js
@@ -0,0 +1,27 @@
+const pool = require('../config/database')
+const { randomUUID } = require('crypto')
+
+class Job {
+    static async create(employerId , jobData) {
+        const id = randomUUID()
+
+        const query = `INSERT INTO jobs (id, employer_id, status, job_title, description, requirements, location, job_type)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?)`
+
+        await pool.query(query, [
+            id,
+            employerId,
+            'active',
+            jobData.job_title,
+            jobData.description,
+            jobData.requirements,
+            jobData.location,
+            jobData.job_type
+        ])
+
+        return {id, employer_id : employerId , ...jobData}
+    }
+
+}
+
+module.exports = Job
