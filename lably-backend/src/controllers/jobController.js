f0b58071deefa0b1509b602e8dc9542247a5d035
MahmoudAlHaj4 <mahmoudalhaj509@gmail.com>
2026-03-01 00:38:37 +0200

    feat: add updatejob and delete job to job controller

--- lably-backend/src/controllers/jobController.js
+++ lably-backend/src/controllers/jobController.js
@@ -37,6 +37,62 @@ async function getEmployerJobs(req, res) {
     }
 }
 
+async function getJob (req, res) {
+    try{
+        const jobId = req.params.id
+        
+        const data = await Job.getOneJob(jobId)
 
+        return res.status(200).json({message: 'success' , data: data})
+    }catch(error) {
+        return res.status(500).json({message : error.message})
+    }
+}
+
+async function updateJob(req, res) {
+    try{
+        const {status, job_title, description, requirements, location, job_type} = req.body
+        const jobId = req.params.id
+
+        const data = await Job.update(jobId , {
+            status : status,
+            job_title : job_title,
+            description: description,
+            requirements: requirements,
+            location: location,
+            job_type: job_type
+        })
+
+        if(!data){
+            return res.status(404).json({message: "Job not found"})
+        }
+        return res.status(200).json({
+            message: "Success" ,
+            data: data
+        })
+    }catch(error){
+        return res.status(500).json({message: error.message})
+    }
+}
+
+async function deleteJob(req, res) {
+    try{
+        const jobId = req.params.id    
+
+        const is_found  = await Job.getOneJob(jobId)
+        if(!is_found) {
+            return res.status(404).json({message : 'Job Not found'})
+        }
+
+        const data = await Job.delete(jobId)
+
+        return res.status(200).json({
+            message : "Job deleted Successfully ",
+            data : data
+        })
+    }catch(error) {
+        return res.status(500).json({message: error.message})
+    }
+}
 
 module.exports = {createJob , getEmployerJobs, getJob, updateJob , deleteJob}
