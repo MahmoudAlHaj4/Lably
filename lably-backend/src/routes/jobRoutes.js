f5c3d315181c619624fc6349ae7051eef3f7aea5
MahmoudAlHaj4 <mahmoudalhaj509@gmail.com>
2026-03-01 00:39:16 +0200

    feat: create job routes with GET POST PUT DELETE endpoints

---
+++ lably-backend/src/routes/jobRoutes.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const { authMiddleware } = require('../middleware/authMiddleware')
+const { createJob, getEmployerJobs, getJob, updateJob, deleteJob } = require('../controllers/jobController')
+const { employerMiddleware } = require('../middleware/employerMiddleware')
+const router = express.Router()
+
+router.post('/job', authMiddleware, employerMiddleware , createJob)
+router.get('/job', authMiddleware , getEmployerJobs)
+router.get('/job/:id', authMiddleware, getJob)
+router.put('/job/:id', authMiddleware, employerMiddleware , updateJob)
+router.delete('/job/:id', authMiddleware, employerMiddleware , deleteJob)
+
+module.exports = router
