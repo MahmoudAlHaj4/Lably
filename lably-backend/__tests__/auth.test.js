a4dab38fe2e79366c5184ab4ef88c9b960d2b9cb
MahmoudAlHaj4 <mahmoudalhaj509@gmail.com>
2026-03-02 01:51:21 +0200

    test: add auth login tests

---
+++ lably-backend/__tests__/auth.test.js
@@ -0,0 +1,46 @@
+const superTest = require("supertest")
+const app = require('../app')
+const pool = require('../src/config/database')
+
+const request = superTest(app)
+
+describe('Auth', ()=>{
+    beforeAll(async ()=>{
+        await request.post('/api/auth/register').send({
+            email: "test@gmail.com",
+            password: "test123@"
+        })
+    })
+
+    afterAll(async()=>{
+        await pool.query('DELETE FROM users WHERE email = ?', ['test@gmail.com'])
+        await pool.end()
+    })
+    it('Should login successfully' , async ()=>{
+        const res = await request.post('/api/auth/login').send({
+            email: "test@gmail.com",
+            password: "test123@"
+        })
+        expect(res.status).toBe(200)
+        expect(res.body.token).toBeDefined()
+    })
+
+    it('Should Password Wrong', async()=>{
+        const res = await request.post('/api/auth/login').send({
+            email: "test@gamil.com",
+            password: "test123"
+        })
+        expect(res.status).toBe(401)
+        expect(res.body.message).toBe('Invalid credentials')
+    })
+
+    it('Should Email Wrong', async ()=>{
+        const res = await request.post('/api/auth/login').send({
+            email: "tests@gail.com",
+            password: "test123@"
+        })
+
+        expect(res.status).toBe(401)
+        expect(res.body.message).toBe("Invalid credentials")
+    })
+})
