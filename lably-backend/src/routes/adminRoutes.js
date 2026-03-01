2e28dac2e1ee371cfdf1393161c5a6f20af47a52
MahmoudAlHaj4 <mahmoudalhaj509@gmail.com>
2026-03-01 02:10:36 +0200

    feat: add reject endpoint in admin Routes

--- lably-backend/src/routes/adminRoutes.js
+++ lably-backend/src/routes/adminRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const { authMiddleware, adminMiddleware } = require('../middleware/authMiddleware')
-const {getAllPendingApplications, getPendingApplication, approvePendingApplication} = require('../controllers/adminController')
+const {getAllPendingApplications, getPendingApplication, approvePendingApplication, rejectPendingApplication} = require('../controllers/adminController')
 const router = express.Router()
 
 router.get('/pending-applications', authMiddleware , adminMiddleware , getAllPendingApplications)
 router.get('/pending-application/:id', authMiddleware , adminMiddleware , getPendingApplication)
 router.put('/approve/:id', authMiddleware, adminMiddleware, approvePendingApplication)
+router.put('/reject/:id', authMiddleware , adminMiddleware ,rejectPendingApplication)
 
 module.exports = router
