f025a778df3ed4527c81d8f50abda1ed30e3efcf
MahmoudAlHaj4 <mahmoudalhaj509@gmail.com>
2026-03-01 21:26:26 +0200

    feat: add Get endpoint to applications routes

--- lably-backend/src/routes/applicationRoutes.js
+++ lably-backend/src/routes/applicationRoutes.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const { authMiddleware } = require('../middleware/authMiddleware')
-const { apply, getEmployerApplication } = require('../controllers/applicationController')
+const { apply, getEmployerApplication, getEmployerApplications } = require('../controllers/applicationController')
+const { jobSeekerMiddleware } = require('../middleware/jobSeekerMiddleware')
+const { employerMiddleware } = require('../middleware/employerMiddleware')
 const router = express.Router() 
 
-router.post('/application/:id', authMiddleware , apply)
-router.get('/application/:id', authMiddleware , getEmployerApplication)
+router.post('/application/:id', authMiddleware, jobSeekerMiddleware , apply)
+router.get('/application/:id', authMiddleware,employerMiddleware , getEmployerApplication)
+router.get('/application', authMiddleware, employerMiddleware , getEmployerApplications)
 
 module.exports = router
