868efcab1e0ec99d9611451db91408ba58aa9957
MahmoudAlHaj4 <mahmoudalhaj509@gmail.com>
2026-03-01 02:29:33 +0200

    feat: add GET endpoint

--- lably-backend/src/routes/employerprofileRoutes.js
+++ lably-backend/src/routes/employerprofileRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const { authMiddleware } = require('../middleware/authMiddleware')
-const { createEmployerProfile, updateEmployerProfile } = require('../controllers/employerProfileController')
+const { createEmployerProfile, updateEmployerProfile, getEmployerProfile } = require('../controllers/employerProfileController')
 const router = express.Router()
 
 router.post('/employer-profile', authMiddleware , createEmployerProfile)
 router.put('/employer-profile', authMiddleware, updateEmployerProfile)
+router.get('/employer-profile', authMiddleware , getEmployerProfile)
 
 module.exports = router
