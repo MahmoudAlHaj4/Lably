c6512f9eb181f4f83115f8f2338de9c4dec56f36
MahmoudAlHaj4 <mahmoudalhaj509@gmail.com>
2026-03-05 06:08:58 +0200

    refactor: clean up job seeker profile endpoints

--- lably-backend/src/routes/jobSeekerProfileRoutes.js
+++ lably-backend/src/routes/jobSeekerProfileRoutes.js
@@ -16,8 +16,8 @@ const { jobSeekerMiddleware } = require('../middleware/jobSeekerMiddleware')
 const router = express.Router()
 
 
-router.post('/job-seeker-profile', authMiddleware ,jobSeekerMiddleware, createJobSeekerProfile)
-router.put('/job-seeker-profile', authMiddleware ,jobSeekerMiddleware, updateJobSeekerProfile)
-router.get('/job-seeker-profile', authMiddleware ,jobSeekerMiddleware, getProfile)
+router.post('/profile', authMiddleware , createJobSeekerProfile)
+router.put('/profile', authMiddleware ,jobSeekerMiddleware, updateJobSeekerProfile)
+router.get('/profile', authMiddleware ,jobSeekerMiddleware, getProfile)
 
 module.exports = router
