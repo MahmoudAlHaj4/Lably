bbcbc394b97b5956d3d0fd68bbf2b5489c51c69c
MahmoudAlHaj4 <mahmoudalhaj509@gmail.com>
2026-03-05 03:06:53 +0200

    test: add get emloyer job, get job ,update job, delete, job tests

--- lably-backend/__tests__/jobs.test.js
+++ lably-backend/__tests__/jobs.test.js
@@ -5,6 +5,7 @@ const request = superTest(app)
 
 describe('Jobs', ()=>{
     let token
+    let jobId
 
     beforeAll( async () => {
         await request.post('/api/auth/register')
@@ -38,7 +39,42 @@ describe('Jobs', ()=>{
             job_type:"on_site"
 
         })
-
         expect(res.status).toBe(201)
+        jobId = res.body.data.id
+    })
+
+    it('Should get Employer Jobs' , async () => {
+        const res = await request.get('/api/jobs/job')
+        .set('Authorization', `Bearer ${token}`)
+
+        expect(res.status).toBe(200)
+    })
+
+    it('Should Get a Job ', async () => {
+        const res= await request.get(`/api/jobs/job/${jobId}`)
+        .set('Authorization', `Bearer ${token}`)
+
+        expect(res.status).toBe(200)
+    })
+
+    it("Should Update A Job", async () => {
+        const res = await request.put(`/api/jobs/job/${jobId}`)
+        .set('Authorization', `Bearer ${token}`)
+        .send({
+            job_title: 'test update',
+            description: "test description",
+            requirements: "test requirements",
+            location: 'test location',
+            job_type:"on_site"
+        })
+
+        expect(res.status).toBe(200)
+    })
+
+    it('Should Delete A Job', async () => {
+        const res = await request.delete(`/api/jobs/job/${jobId}`)
+        .set('Authorization', `Bearer ${token}`)
+
+        expect(res.status).toBe(200)
     })
 })
