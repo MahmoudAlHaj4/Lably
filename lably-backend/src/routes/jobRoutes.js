58bf1a557e9555e49224031c55787b741b5c537e
MahmoudAlHaj4 <mahmoudalhaj509@gmail.com>
2026-03-05 06:09:56 +0200

    refactor: clean up jobs endpoints

--- lably-backend/src/routes/jobRoutes.js
+++ lably-backend/src/routes/jobRoutes.js
@@ -16,10 +16,10 @@ const { createJob, getEmployerJobs, getJob, updateJob, deleteJob } = require('..
 const { employerMiddleware } = require('../middleware/employerMiddleware')
 const router = express.Router()
 
-router.post('/job', authMiddleware, employerMiddleware , createJob)
-router.get('/job', authMiddleware ,employerMiddleware, getEmployerJobs)
-router.get('/job/:id', authMiddleware, getJob)
-router.put('/job/:id', authMiddleware, employerMiddleware , updateJob)
-router.delete('/job/:id', authMiddleware, employerMiddleware , deleteJob)
+router.post('/jobs', authMiddleware, employerMiddleware , createJob) 
+router.get('/jobs', authMiddleware ,employerMiddleware, getEmployerJobs)
+router.get('/jobs/:id', authMiddleware, getJob)
+router.put('/jobs/:id', authMiddleware, employerMiddleware , updateJob)
+router.delete('/jobs/:id', authMiddleware, employerMiddleware , deleteJob)
 
 module.exports = router
