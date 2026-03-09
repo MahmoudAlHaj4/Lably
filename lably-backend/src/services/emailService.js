76307d94b891d68939fb82d86e30f0b4f1f24530
MahmoudAlHaj4 <mahmoudalhaj509@gmail.com>
2026-03-09 09:01:52 +0200

    fix: change secure to true

--- lably-backend/src/services/emailService.js
+++ lably-backend/src/services/emailService.js
@@ -3,7 +3,7 @@ const nodemailer = require('nodemailer')
 const transporter = nodemailer.createTransport({
   host: process.env.BREVO_HOST,
   port: process.env.BREVO_PORT,
-  secure: false,
+  secure: true,
   auth: {
     user: process.env.BREVO_USER,
     pass: process.env.BREVO_PASS
