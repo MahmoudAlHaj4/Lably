35c9adf53c64ba650e8be42dd6ef79c6561e42be
MahmoudAlHaj4 <mahmoudalhaj509@gmail.com>
2026-03-11 07:21:02 +0200

    feat: add lably domain to CROS

--- lably-backend/src/middleware/crosMiddleware.js
+++ lably-backend/src/middleware/crosMiddleware.js
@@ -12,7 +12,7 @@
 
 const corsMiddleware = (req, res, next) => {
 
-   res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5500')
+   res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5500' , 'https://lably-three.vercel.app/')
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
     if (req.method === 'OPTIONS') {
